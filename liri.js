c1546b9f38afacd03f8183612abe15702523f565
mohak92 <tamhane.mohak@gmail.com>
2019-11-02 07:25:36 -0400

    global vars created

--- liri.js
+++ liri.js
@@ -1,1 +1,8 @@
 require("dotenv").config();
+var keys = require("./keys.js");
+var axios = require("axios");
+var fs = require("fs");
+var queryType = process.argv[2];
+var requestInfo = process.argv.splice(3).join(" ").replace(/"/g, "");
+var loggerInfo;
+var url;
